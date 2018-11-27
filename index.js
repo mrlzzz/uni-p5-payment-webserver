4c36107bd7869ff731be8fcddd05162fcfc7bdfc
mrlzzz <morelzzzz@gmail.com>
2018-11-27 17:48:34 +0100

    make

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+
+const port = process.env.PORT
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+let one = 'to ja :D';
+let two = '';
+
+app.get('/', function(req, res){
+
+    res.send("One: " + one);
+
+});
+
+
+
+
+app.post("/post", function(req, res){
+
+    one = req.body.name;
+    two = req.body.text;
+
+    
+
+    return;
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
