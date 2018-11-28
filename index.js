1295d6d0519921c0d950c36d2bb030eaf87d5b75
mrlzzz <morelzzzz@gmail.com>
2018-11-28 16:16:25 +0100

    jajaebie

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 const express = require('express')
 const app = express()
 
-
 const port = 3000
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
+
 let one = 'to ja :D';
-let two = '';
+let two = "";
 
 app.get('/', function(req, res){
-
     res.send("One: " + one);
-
 });
 
-
-
-
 app.post("/post", function(req, res){
-
     one = req.body.name;
     two = req.body.text;
-
-    
-
-    return;
+    console.log("POST request received.")
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
