3aa1425d81c1d801163c1fe1379ce21180efd93e
mrlzzz <morelzzzz@gmail.com>
2018-12-12 15:37:14 +0100

    usp

--- index.js
+++ index.js
@@ -23,14 +23,59 @@ admin.initializeApp({
   databaseURL: "https://paymentapp-fd05e.firebaseio.com"
 });
 
-function pushToDb(data){
+function pushToDb(timestamp, pin){
 
-	admin.database().ref('/').set({
-    	username: data
-    	
-	});
+	admin.database().ref("/transactions").set(
+	
+		{
+		"Timestamp": timestamp,
+		"PIN": pin
+		}
+	
+	);
 
 }
+
+
+admin.database().ref('/').set({
+    	users: [
+			{
+				"CreditCard": {
+				    "IssuingNetwork": "Master Card",
+				    "CardNumber": "5270820790587748",
+				    "Name": "Landon Carter",
+				    "CVV": "821",
+				    "Limit": "216$",
+				    "Exp": "01/2020",
+				    "PIN": "1234"
+				}
+			},
+			{
+				"CreditCard": {
+				    "IssuingNetwork": "Master Card",
+				    "CardNumber": "5159191881143965",
+				    "Name": "Aiden Perez",
+				    "CVV": "101",
+				    "Limit": "1228$",
+				    "Exp": "05/2021",
+				    "PIN": "5685"
+				}
+			},
+			{
+				"CreditCard": {
+				    "IssuingNetwork": "Master Card",
+				    "CardNumber": "5118215196387353",
+				    "Name": "James Evans",
+				    "CVV": "791",
+				    "Limit": "522$",
+				    "Exp": "01/2023",
+				    "PIN": "1010"
+				}
+			}
+		],
+		
+		transactions: ""
+    });
 
 
 
@@ -42,7 +87,8 @@ function pushToDb(data){
 //logic
 
 let response = "Unknown, send me something from QT";
-let result = "";
+let timestamp = "";
+let pin = "";
 
 function verify(r){
 
@@ -63,12 +109,41 @@ app.get('/', function(req, res){
 });
 
 app.post("/post", function(req, res){
-    result = req.body.result;
+    timestamp = req.body.timestamp;
+    pin = req.body.pin;
     
-	pushToDb(result);
+	pushToDb(timestamp, pin);
     console.log("POST request received.")
 
 	res.send("Sent to DB, hopefully");
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
